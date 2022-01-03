374d146cc9781706196ee5ad4d2416b5589a4760
larry <xspanni@gmail.com>
2022-01-03 22:03:12 +0800

    猜数字游戏增加大小比较

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use std::io;
 use rand::Rng;
 use ferris_says::say;
+use std::cmp::Ordering;
 use std::io::{stdout, BufWriter, Read};
+use libc::printf;
 
 extern crate libc;
 
@@ -36,7 +38,14 @@ fn guess_number() {
     let mut guess = String::new();
     io::stdin().read_line(&mut guess).expect("Failed to read line");
 
+    let guess: u32 = guess.trim().parse().expect("Please type a number");
     println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!")
+    }
 }
 
 fn main() {
